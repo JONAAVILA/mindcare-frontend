c95dc70623b3e66ca5be21d118b6612983c6dbed
JONAAVILA <jona_1969@hotmail.com>
2025-03-02 20:01:07 -0300

    blog cards created

--- src/components/adapters/getBlogs.js
+++ src/components/adapters/getBlogs.js
@@ -4,9 +4,7 @@ const { PUBLIC_URL_GET_BLOGS } = import.meta.env
 
 const getBlogs = async ()=>{
 
-    const response = await axios.get(PUBLIC_URL_GET_BLOGS,{
-        withCredentials:true
-    })
+    const response = await axios.get(PUBLIC_URL_GET_BLOGS)
 
     return response.data
 }
